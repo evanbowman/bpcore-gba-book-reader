aa9e4e56de84df44d4f7408b45044d60d9e8a9ff
evanbowman <ebowman@bu.edu>
2021-09-10 20:59:30 -0400

    it sort of works

--- main.lua
+++ main.lua
@@ -15,6 +15,19 @@ local page = 0
 local read_index = 0
 
 
+-- Note about load_offset/store_offset: We need to be able to backtrack to
+-- previous pages, which is much simpler if we store the byte offset of the
+-- beginning of the page each time we load a new one.
+function store_index()
+   poke4(_IRAM + page * 4, read_index)
+end
+
+
+function load_index(pg)
+   return peek4(_IRAM + pg * 4)
+end
+
+
 function read_char()
    local value = string.char(peek(book_data + read_index))
    read_index = read_index + 1
@@ -29,58 +42,50 @@ end
 
 function read_word()
    local word = ""
+   local delim = ""
    while true do
       local chr = read_char()
-      if chr ~= "\r" then -- skip carriage-return
+      if chr ~= "\r" then
          if is_delimiter(chr) then
-            return word, chr
+            return word, delim .. chr
          end
          word = word .. chr
+      else
+         delim = delim .. chr
       end
    end
 end
 
 
-local carry_word = nil
-local carry_delim = nil
-
 local x_write = 1
 local y_write = 2
 
 
-function handle_delimiter(delim)
-   if delim == "\n" then
+function handle_delimiter(delim, print_fn)
+   if delim == "\n" or delim == "\r\n" then
       y_write = y_write + 2
       x_write = 1
    elseif delim == " " then
-      print(delim, x_write, y_write, text_color, pg_color)
+      print_fn(delim, x_write, y_write)
       x_write = x_write + 1
    end
 end
 
 
-function load_page(pg)
+function load_page(pg, print_fn)
 
    page = pg
 
-   fill(0, 0) -- clear all chars on the page
-   clear()
-   display()
+   store_index()
+
 
    local page_num = tostring(pg + 1)
-   print(page_num,  (29 - string.len(page_num)), 18, text_color, pg_color)
+   print_fn(page_num, (29 - string.len(page_num)), 18)
+   -- print(page_num,  , text_color, pg_color)
 
 
    x_write = 1
    y_write = 1
-
-   if carry_word then
-      print(carry_word, x_write, y_write, text_color, pg_color)
-      x_write = x_write + utf8.len(carry_word)
-      handle_delimiter(carry_delim)
-      carry_word = nil
-      carry_delim = nil
-   end
 
    while true do
       word, delim = read_word()
@@ -95,36 +100,56 @@ function load_page(pg)
          y_write = y_write + 2
 
          if y_write >= 17 then
-            carry_word = word
-            carry_delim = delim
+            -- We ran out of room, subtract the read-offset by the length of the
+            -- last processed word.
+            read_index = read_index - (string.len(word) + string.len(delim))
             return
          end
       end
 
-      print(word, x_write, y_write, text_color, pg_color)
+      print_fn(word, x_write, y_write)
       x_write = x_write + word_len
 
-      handle_delimiter(delim)
+      handle_delimiter(delim, print_fn)
    end
 end
 
 
-load_page(0)
+function active_print_fn(str, x, y)
+   print(str, x, y, text_color, pg_color)
+end
+
+
+function void_print_fn(str, x, y)
+   -- Do nothing...
+end
+
+
+load_page(0, active_print_fn)
 
 
 print(tostring(collectgarbage("count") * 1024), 0, 19, text_color, pg_color)
-print("hello.", 0, 18, text_color, pg_color)
 
 
 while true do
    clear()
 
-   if btnp(4) then
-      load_page(page - 1)
+   if btnp(4) and page > 0 then
+      fill(0, 0) -- clear all chars on the page
+      clear()
+      display()
+
+      read_index = load_index(page - 1)
+      load_page(page - 1, active_print_fn)
    end
 
    if btnp(5) then
-      load_page(page + 1)
+
+      fill(0, 0) -- clear all chars on the page
+      clear()
+      display()
+
+      load_page(page + 1, active_print_fn)
    end
 
    display()
